808232ea7f68fe2ed068c57d00dd183464b502bb
Charles McCarthy <charles@studybundles.com>
2019-03-18 14:36:13 -0400

    Empty Pages & MainWindow Layout

--- TaskManagementApp/MainWindow.xaml.cs
+++ TaskManagementApp/MainWindow.xaml.cs
@@ -12,14 +12,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TaskManagementApp.Models;
+using TaskManagementApp.Pages;
+using TaskManagementApp.Services;
 
 namespace TaskManagementApp {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        public TaskManager TaskManager { get; set; }
+
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            TaskManager = new TaskManager(FakeDatabase.GetTasks());
+            SetHeadingText("Tasks");
+
+            NavigateHome();
+        }
+
+        private void SetHeadingText(string heading) => txtBlkHeading.Text = heading;
+
+        private void TxtBlkTasks_Click(object sender, RoutedEventArgs e) => NavigateHome();
+
+        private void TxtBlkAddTask_Click(object sender, RoutedEventArgs e) => NavigateAddTask();
+
+        private void NavigateHome() => mainFrame.NavigationService.Navigate(new TaskManagerPage());
+
+        private void NavigateAddTask() => mainFrame.NavigationService.Navigate(new AddTaskPage());
+
     }
 }
---
+++ TaskManagementApp/Services/FakeDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskManagementApp.Models;
+
+namespace TaskManagementApp.Services {
+    public static class FakeDatabase {
+        
+        public static List<Task> GetTasks() {
+            return new List<Task>();
+        }
+
+    }
+}
