7e3804ed0b125783509e91216ae071a6f5b64ef5
Charles McCarthy <charles@studybundles.com>
2019-03-18 14:00:04 -0400

    Task & TaskManager Class

---
+++ TaskManagementApp/Models/Task.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp.Models { 
+    public enum Priority {
+        High,
+        Medium,
+        Low
+    }
+
+    public enum Category {
+        ShopFloor,
+        StockRoom,
+        LoadingBay,
+        Management
+    }
+
+    public class Task {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public Category Category { get; private set; }
+        public Priority Priority { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public string Responsibility { get; private set; }
+        public string[] Labels { get; private set; }
+        public bool IsComplete { get; private set; }
+
+        public Task(string title, string description, Category category,
+            Priority priority, DateTime dueDate, string responsibility, string[] labels): this(title, description, category, priority, dueDate, labels) {
+            Responsibility = responsibility;
+        }
+
+        public Task(string title, string description, Category category,
+            Priority priority, DateTime dueDate, string[] labels) {
+            Title = title;
+            Description = description;
+            Category = category;
+            Priority = priority;
+            DueDate = dueDate;
+            Labels = labels;
+        }
+
+        public void CompleteTask() => IsComplete = true;
+
+        public void AssignResponsibility(string name) => Responsibility = name;
+    }
+}
---
+++ TaskManagementApp/Models/TaskManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp.Models {
+    public class TaskManager {
+        public ObservableCollection<Task> Tasks { get; private set; }
+
+        public TaskManager(List<Task> tasks) {
+            Tasks = new ObservableCollection<Task>(tasks);
+        }
+
+        public void AddTask(Task t) => Tasks.Add(t);
+
+        public void DeleteTask(Task t) => Tasks.Remove(t);
+    }
+}
